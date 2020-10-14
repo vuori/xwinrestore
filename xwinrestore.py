66c94e82f4e7d0af4c8d353098c3e5d922338534
vuori <vuori@notcom.org>
2020-10-15 00:55:46 +0300

    Added license.

--- xwinrestore.py
+++ xwinrestore.py
@@ -4,7 +4,23 @@
 """Remembers window positions for different display configurations.
 
 Recommended usage is to start this on login from your desktop environment's
-autostart."""
+autostart.
+
+    Copyright 2020 Valtteri Vuorikoski
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 
 import logging
 import struct
